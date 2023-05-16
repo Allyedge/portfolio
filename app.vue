221842da1a418a3a4fcdba30997cbc6acec8763e
Allyedge <alimarslank@gmail.com>
2023-05-16 16:31:46 +0200

    Update project layout and nuxt version

--- app.vue
+++ app.vue
@@ -1,12 +1,7 @@
 <template>
-  <Home />
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
-
-<style>
-body {
-  font-family: Inter, ui-sans-serif, system-ui, -apple-system,
-    BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans",
-    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
-    "Noto Color Emoji";
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -36,3 +36,12 @@
     <Footer />
   </div>
 </template>
+
+<style>
+body {
+  font-family: Inter, ui-sans-serif, system-ui, -apple-system,
+    BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans",
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+    "Noto Color Emoji";
+}
+</style>
